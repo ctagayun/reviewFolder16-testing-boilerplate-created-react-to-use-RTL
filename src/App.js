d5a69e10c7137bc4ce24ff55699096619c9bc050
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-09-28 11:10:39 -0500

    Completed setting up JEST, Webpack, and Babel

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const title = 'Hello React - This Rendering a Component Using RTL From src/App.js';
+
+function App() {
+  return <div>{title}</div>;
+}
+
+export default App;
